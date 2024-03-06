1341007a06f8a7f2f6d55c1c1d228bc5ae3bbac4
nwiuser <152625420+nwiuser@users.noreply.github.com>
2024-03-06 19:02:19 +0100

    Add files via upload

---
+++ nltk_0.py
@@ -0,0 +1,15 @@
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_word):
+    pass
+
+def word_tokenize(sentence):
+    return None
